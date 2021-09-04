23946653acd11b1ca399d19e834e41efc1e5237d
RayaneBelaid <belaidraya@gmail.com>
2021-09-04 11:51:02 +0100

    Add Flask basic routes

--- 02-Flask-Basics/01-Basic-Routes.py
+++ 02-Flask-Basics/01-Basic-Routes.py
@@ -1,11 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #127.0.0:500
 def index():
     return '<h1>Hello Puppy!</h1>'
 
-@app.route('/information')
+@app.route('/information') #127.0.0:5000/information
 def info():
     return '<h1>Puppies are cute!</h1>'
 
